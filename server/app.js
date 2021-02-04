6064dc229a5bd17cfaa49dd579ac1517d73db3bb
Rizki Candra <mhdrizkicandra@gmail.com>
2021-02-04 20:33:23 +0700

    perbaikan controller helper dan model

--- server/app.js
+++ server/app.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const cors = require('cors')
+const routes = require('./routes/index.js')
 const app = express()
 const PORT = 3000
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/', routes)
+
 
 app.listen(PORT, () => console.log(`server running on port : ${PORT}`))
