32e5f132a681958a252d2c5df7a51e9e063c1ade
amoswijaya <amoswijaya728@gmail.com>
2021-02-04 23:45:00 +0700

    errorHandling done

---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,16 @@
+const errorHandler = (err, req, res,next) =>{
+    if(err.name === 'SequelizeUniqueConstraintError'){
+       return res.status(400).json({error: 'Email already exist'})
+    } else if(err.name == "SequelizeValidationError"){
+        const errors = err.errors.map(e => e.message)
+       return res.status(400).json({errors: errors}) 
+    }else if(err.name == 'customError'){
+       return res.status(err.code).json({err: err.msg})
+    }else if(err.name == 'JsonWebTokenError'){
+       return res.status(401).json({err: 'Invalid token'}) 
+    }else{
+       return res.status(500).json({error: 'Internal Server Error'})
+    }
+}
+
+module.exports = errorHandler
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,8 @@
-const routes = require('express').Router()
 const router = require('express').Router()
 const food = require('./food')
 const user = require('./user')
 
-router.use('/', food)
+router.use('/foods', food)
 router.use('/users', user)
 
 module.exports = router
